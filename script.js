e4903d93ff33df4c4f539c1bdd0415913f4bd80c
agasca <contacto@pegoga.com>
2020-02-17 17:55:12 -0600

    def. this

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const c = console.log
 c("inicio")
 
 /*
+ejemplos fuente en edteam
 let cadena = `alberto`
 //
 //ejemplo01 declaracion funcion
@@ -80,15 +81,107 @@ const sumaFlecha = x => y => x + y
     //}
     c(suma(2)(7))   //el primer parentesis para el primer return, el segundo parentesis para el segundo return
     c(sumaFlecha(2)(7))   //el primer parentesis para el primer return, el segundo parentesis para el segundo return
+    
+    const doSomething = x => y => x * y
+    c(doSomething)              //x => y => x * y
+    const a = doSomething(2)(3) //6
+    c(a)
+    const b = doSomething(4)    //y => x * y //se quedo en el primer parametro
+    c(b)
+    c(doSomething(a)(b(3)))     //1.- a = 6, 2.-  y=>4*y es decir y=>4*3, 3.-multiplica los resultados 6*12 = 72
+    //
+    
+    //
+    //programacion funcional
+    //
+    //no es una funcion pura, dependen del scope/ambito
+    let a = `hola`
+    const saludo = persona => { 
+    a = a + ' ' + persona
+    return a
+}
+c(saludo('beto'))
+c(a) //modifico algo que etsba afuera (ambito), no es una funcion pura
+//
+//funcion pura
+let ap = `holaP`
+const saludoP = (saludo, personaP) => `${saludo} ${personaP}`  //es una funcion pura
+c(saludoP(ap, 'betoP'))
+c(ap)
+
+//
+//autoinvocadas (sin parentesis)
+let aI = `HolaAI `
+const saludoAI = ( (saludo, personaP) => `${saludo} ${personaP}` )(aI,`Luis`)  //tiene un parentesis exterior vs las pura, se convierte en una expresion
+c(saludoAI)
+
+//
+//funciones nonmbradas y anonimas
+const saludoP = (saludo, personaP) => `${saludo} ${personaP}`  //es una funcion pura
+//se ejecuta nombrandola
+c(saludoP())
+//funcion callback anonima, no tiene nombre
+setTimeout(()=>{
+    alert(`hola`)
+},3000)
+
+//
+//scope o ambito
+{
+    let profesor = "ag"
+    {
+        c(profesor)
+    }
+}
+
+c(profesor)
+
+//
+//closures
+//a travers de funicones internas llegar a valores externos
+function aumentar(){
+    let numero = 0
+    return function(){
+        numero++
+        c(numero)
+    }
+}
+aumentar()      //no hizo nada por...  que solo llego oal primer return
+aumentar()()    //1
+aumentar()()    //1
+aumentar()()    //1
+aumentar()()    //1, no suma
+const incrementar = aumentar()  //tiene el valor de retorno de F(aumentar, 
+//su valor de retorno es `return function(){numero++; c(numero)}` 
+//y esta funcion hace aumentar su valor en uno al valor de un numero.
+//ese numero esta por fuera de la funcion de retorno es `return function(){numero++; c(numero)}` 
+//incrementar hace la accion de agregar el valor en uno
+//como si fuese una funcion no pura
+numero++
+c(numero)
+}
+incrementar()   //1
+incrementar()   //2
+incrementar()   //3
+incrementar()   //4
 */
 
-const doSomething = x => y => x * y
-c(doSomething)              //x => y => x * y
-const a = doSomething(2)(3) //6
-c(a)
-const b = doSomething(4)    //y => x * y //se quedo en el primer parametro
-c(b)
-c(doSomething(a)(b(3)))     //1.- a = 6, 2.-  y=>4*y es decir y=>4*3, 3.-multiplica los resultados 6*12 = 72
 //
+//this
+//representa al objeto del cual se ejecuta la funcion}las arrow functions permiten acceder al this superior dentro de un closure (lexical this)
+let edad = 10
+let user = {
+    nomnre: `al`,
+    edad: 54,
+    getEdad1(){
+        c(this.edad)
+    },
+    getEdad2(){
+        c(edad) //no tiene this por lo que toma del scope mas arriba
+    }
+
+}
+user.getEdad1()
+user.getEdad2()
 
 c("fin")
