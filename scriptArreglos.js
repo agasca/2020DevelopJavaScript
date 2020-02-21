ed27c5f1011dffb68ea275a0de673db4ba4604ce
agasca <contacto@pegoga.com>
2020-02-21 12:11:24 -0600

    def. arreglos cont

--- scriptArreglos.js
+++ scriptArreglos.js
@@ -22,3 +22,80 @@ c(s1)
 c(s2)
 c(s3)
 c(s4)
+
+//
+//agregat oy quitar valores
+let arr = [1,2,3,4]
+arr.push(5)
+c(arr)
+c(arr.length)
+
+//
+//quitar elemento (el ultimo)
+arr.pop()
+c(arr)
+c(arr.length)
+
+//
+//al inicio del array
+arr.unshift(`nuevo`)
+c(arr)
+c(arr.length)
+
+//
+//elimina el primer valor
+arr.shift()
+c(arr)
+c(arr.length)
+
+//arreglo de texto
+arrTexto = [`peru`, `colombia`, `mexico`, `bolivia`]
+c(arrTexto)
+c(arrTexto.length)
+
+//splice posicion personalizada
+arrTexto.splice(2,0, `chile`, `ecuador`)
+c(arrTexto)
+c(arrTexto.length)
+
+arrTexto.splice(4,1, `costa ricac`)
+c(arrTexto)
+c(arrTexto.length)
+
+
+//eliminar personalizada
+arrTexto.splice(1,1)
+c(arrTexto)
+c(arrTexto.length)
+
+//extraer
+arrTexto.slice(2,4)
+c(arrTexto)
+c(arrTexto.length)
+
+//invertir
+let sal = 'hola mama'
+c(sal.split('').reverse(''))
+c(sal.split('').reverse('').join(''))
+
+const revreseText = cadena => cadena.split('').reverse('').join('')
+c(revreseText(`mexico`))
+
+//ordenar no opera con numeros
+let arr2 = ['B', 'A', 'Z', 'F']
+c(arr2.sort())
+c(arr2.sort().reverse())
+
+//ordenar numeros
+let numbers = [1,10,100,35,45,300]
+c(numbers.sort((a,b)=> a-b)) //algoritmo
+
+let numbers2 = [99,98,46,34,0]
+//concatenar
+//.concat 
+c(numbers.concat(numbers2))
+c(numbers2.concat(7,17,27))
+
+//.join convierte numero a cadena con delimitador
+c(numbers.join('|'))
+
